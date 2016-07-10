bb528c24521a248cac865ff5901aac20e9e1a677
David S. Jackson <deepbsd@yahoo.com>
2016-07-09 21:42:04 -0400

    aupdating app with forms partials and specs

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,6 +3,37 @@ class PinsController < ApplicationController
   def index
     @pins = Pin.all
   end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.create(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @pin = Pin.update(params[:id], pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :edit
+    end
+  end
   
   def show
     @pin = Pin.find(params[:id])
@@ -12,5 +43,11 @@ class PinsController < ApplicationController
     @pin = Pin.find_by_slug(params[:slug])
     render :show
   end
-  
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
